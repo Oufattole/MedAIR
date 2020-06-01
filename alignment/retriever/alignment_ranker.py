18b960d673a94ced7bf45609b2683f860c387f21
nassim <nassim@mit.edu>
2020-05-31 21:34:43 -0500

    final try

--- alignment/retriever/alignment_ranker.py
+++ alignment/retriever/alignment_ranker.py
@@ -38,6 +38,12 @@ logger.addHandler(console)
 shared_matrix = None
 shared_matrix_shape = None
 
+
+def init(matrix, shape):
+    global shared_matrix, shared_matrix_shape
+    shared_matrix = matrix
+    shared_matrix_shape = shape
+
 class AlignmentDocRanker(object):
     """Loads a pre-weighted inverted index of token/document terms.
     Scores new queries by taking sparse dot products.
@@ -57,12 +63,10 @@ class AlignmentDocRanker(object):
         self.hash_to_ind = {hash_to_ind[i]:i for i in range(hash_to_ind.size)}
         # import pdb; pdb.set_trace()
         logger.info(f'Allocate RawArray')
-        global shared_matrix
-        global shared_matrix_shape
-        shared_matrix = RawArray(c.c_double, matrix.shape[0]*matrix.shape[1])
-        shared_matrix_shape = matrix.shape
+        self.matrix = RawArray(c.c_double, matrix.shape[0]*matrix.shape[1])
+        self.matrix_shape = matrix.shape
         logger.info(f'make wrapper')
-        matrix_wrapper = np.frombuffer(shared_matrix).reshape(matrix.shape)
+        matrix_wrapper = np.frombuffer(self.matrix).reshape(matrix.shape)
         logger.info(f'copy to RawArray')
         np.copyto(matrix_wrapper, matrix[:,:])
         # self.matrix = matrix
@@ -79,64 +83,14 @@ class AlignmentDocRanker(object):
         self.topn = 30
         self.es = Elasticsearch()
 
-    def score_query(self, input_query):
-        """
-        query : question + answer
 
-        returns alignment score
-
-
-        """
-        # logger.debug(f'es_search')
-        global shared_matrix
-        global shared_matrix_shape
-        hash_to_ind, freq_table, matrix = self.hash_to_ind, self.freq_table, np.frombuffer(shared_matrix).reshape(shared_matrix_shape)
-        tokenizer = self.tokenizer
-        es = self.es
-        #formulate search query
-        es_query = Q('match', body=input_query)
-        search = Search(using=es, index="corpus").query(es_query).source(False)[:self.es_topn]
-        hits = search.execute()
-        doc_ids = np.array([int(hit.meta.id) for hit in hits])
-        doc_ids.sort()
-        tokens = list(tokenizer.tokenize(input_query).words()) # globalize tokenize with init
-        valid_hashes = [utils.hash(token, self.hash_size) for token in tokens if utils.hash(token, self.hash_size) in hash_to_ind]
-        valid_hash_inds = np.array([hash_to_ind[token_hash] for token_hash in valid_hashes])
-        valid_hash_inds.sort()
-        # retrieve matrix
-        cos_sim_matrix = matrix[valid_hash_inds[:,None],doc_ids]
-        # logger.debug(f'retrieve up matrix')
-        # cos_sim_matrix = matrix[:,doc_ids][valid_hash_inds,:]
-        # retrieve idfs
-        # logger.debug(f'Calculate the rest')
-        num_docs = matrix.shape[1]
-        Ns = np.array([freq_table[token_hash] for token_hash in valid_hashes]) #doc frequencies array same order as 
-        idfs = np.log((num_docs - Ns + 0.5) / (Ns + 0.5))
-        idfs[idfs < 0] = 0
-
-        assert(cos_sim_matrix.shape[0]==idfs.size)
-        
-        scores = (np.dot(idfs,cos_sim_matrix)) #find correct axis, sum rows
-        topn_scores = scores.argsort()[-self.topn:]
-        logger.info(f'Done')
-        return np.sum(scores[topn_scores])
-
-    def solve_question(self, question_object):
-        options = list(question_object.get_options())
-        queries = [question_object.prompt + " " + option for option in options]
-        scores = [self.score_query(query) for query in queries]
-        high_score = max(scores)
-        search_answer = None
-        for i in range(len(scores)):
-            if scores[i]==high_score:
-                search_answer = options[i]
-        return question_object.is_answer(search_answer)
     def solve_question_set(self, questions):
         total = 0
         correct = 0 
         pbar = tqdm(questions, desc='accuracy', total = len(questions))
-        with Pool(processes=self.num_workers) as pool:
-            for result in pool.imap(self.solve_question, pbar):
+        with Pool(processes=self.num_workers,initializer=init,initargs=(self.matrix,self.matrix_shape)) as pool:
+            _solve = partial(solve_question, self.hash_to_ind, self.freq_table, self.tokenizer, self.es, self.es_topn, self.topn,self.hash_size)
+            for result in pool.imap(_solve, pbar):
                 total +=1
                 correct += 1 if result else 0
                 pbar.set_description(f'accuracy (accuracy={correct/total})')
@@ -159,6 +113,59 @@ class AlignmentDocRanker(object):
         questions = Question.read_jsonl(DATA_DIR + question_filename)
         return self.solve_question_set(questions)
 
+def solve_question(hash_to_ind, freq_table, tokenizer, es, es_topn, topn, hash_size, question_object):
+    options = list(question_object.get_options())
+    queries = [question_object.prompt + " " + option for option in options]
+    _solve = partial(score_query, hash_to_ind, freq_table, tokenizer, es, es_topn, topn, hash_size)
+    scores = [_solve(query) for query in queries]
+    high_score = max(scores)
+    search_answer = None
+    for i in range(len(scores)):
+        if scores[i]==high_score:
+            search_answer = options[i]
+    return question_object.is_answer(search_answer)
+
+def score_query(hash_to_ind, freq_table, tokenizer, es, es_topn, topn, hash_size, input_query):
+    """
+    query : question + answer
+
+    returns alignment score
+
+
+    """
+    # logger.debug(f'es_search')
+    global shared_matrix
+    global shared_matrix_shape
+    matrix = np.frombuffer(shared_matrix).reshape(shared_matrix_shape)
+    # tokenizer = self.tokenizer
+    # es = self.es
+    #formulate search query
+    es_query = Q('match', body=input_query)
+    search = Search(using=es, index="corpus").query(es_query).source(False)[:es_topn]
+    hits = search.execute()
+    doc_ids = np.array([int(hit.meta.id) for hit in hits])
+    doc_ids.sort()
+    tokens = list(tokenizer.tokenize(input_query).words()) # globalize tokenize with init
+    valid_hashes = [utils.hash(token, hash_size) for token in tokens if utils.hash(token, hash_size) in hash_to_ind]
+    valid_hash_inds = np.array([hash_to_ind[token_hash] for token_hash in valid_hashes])
+    valid_hash_inds.sort()
+    # retrieve matrix
+    cos_sim_matrix = matrix[valid_hash_inds[:,None],doc_ids]
+    # logger.debug(f'retrieve up matrix')
+    # cos_sim_matrix = matrix[:,doc_ids][valid_hash_inds,:]
+    # retrieve idfs
+    # logger.debug(f'Calculate the rest')
+    num_docs = matrix.shape[1]
+    Ns = np.array([freq_table[token_hash] for token_hash in valid_hashes]) #doc frequencies array same order as 
+    idfs = np.log((num_docs - Ns + 0.5) / (Ns + 0.5))
+    idfs[idfs < 0] = 0
+
+    assert(cos_sim_matrix.shape[0]==idfs.size)
+    
+    scores = (np.dot(idfs,cos_sim_matrix)) #find correct axis, sum rows
+    topn_scores = scores.argsort()[-topn:]
+    logger.info(f'Done')
+    return np.sum(scores[topn_scores])
 
 
 if __name__ == "__main__":
